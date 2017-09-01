b5951ae029e4d9fe4facbb0dff64eb92284cce0f
Niraj Modi <nirajmodi51@gmail.com>
2017-09-01 10:34:46 +0530

    Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+var {ObjectId} = require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mogoose.js'); 
 var {Todo} = require('./models/todo.js');
 var {Users} = require('./models/user.js');
-
-var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectId.isValid(id)){
+		res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
