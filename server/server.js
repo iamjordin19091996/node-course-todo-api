20b260b3ed66072f20761b7c560d5fb4a28c2996
Niraj Modi <nirajmodi51@gmail.com>
2017-09-04 21:47:32 +0530

    Add POSR /users route and token

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-require(./config/config.js);
+require('./config/config');
 
 const _ = require('lodash');
-const {ObjectId} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mogoose.js'); 
-var {Todo} = require('./models/todo.js');
-var {Users} = require('./models/user.js');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -15,89 +15,103 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	var todo = new Todo({
-		text: req.body.text
-	});
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-	todo.save().then((doc) => {
-		res.send(doc);
-	}, (err) => {
-		res.status(400).send(err);
-	});
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
-		res.send({todos});
-	}, (e) => {
-		res.status(400).send(e);
-	});
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos/:id', (req, res) => {
-	var id = req.params.id;
+  var id = req.params.id;
 
-	if(!ObjectId.isValid(id)){
-		res.status(404).send();
-	}
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-	Todo.findById(id).then((todo) => {
-		if(!todo) {
-			return res.status(404).send();
-		}
-		res.send({todo});
-	}).catch((e) => {
-		res.status(400).send();
-	});
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
-	var id = req.params.id;
+  var id = req.params.id;
 
-	if(!ObjectId.isValid(id)){
-		return res.status(404).send();
-	}
-	
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-	Todo.findByIdAndRemove(id).then((todo) => {
-		if(!todo){
-			return res.status(404).send();
-		}
-		res.send({todo});
-	}).catch((e) => {
-		res.status(400).send();
-	});
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.patch('/todos/:id', (req, res) => {
-	var id = req.params.id;
-	var body = _.pick(req.body, ['text', 'completed']);
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
 
-	if(!ObjectId.isValid(id)){
-		return res.status(400).send();
-	}
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-	if(_.isBoolean(body.completed) && body.completed){
-		body.completedAt = new Date().getTime();
-	} else {
-		body.completed = false;
-		body.completedAt = null;
-	}
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
 
-	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-		if (!todo) {
-			return res.status(404).send();
-		}
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-		res.send({todo});
-	}).catch((e) => {
-		return res.status(400).send();
-	});
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
-	console.log(`Started up at port:${port}`); 
+  console.log(`Started up at port ${port}`);
 });
 
-
-module.exports ={app};
+module.exports = {app};
